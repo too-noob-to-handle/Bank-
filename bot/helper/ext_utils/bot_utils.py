58b19ef8a6c726a779e7a1cc2f01ef0e0b107a36
majnurangeela <84721324+majnurangeela@users.noreply.github.com>
2022-06-08 12:27:21 +0530

    Added DriveBuzz Clone Support

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -243,7 +243,7 @@ def is_unified_link(url: str):
     return bool(url)
 
 def is_udrive_link(url: str):
-    url = re_match(r'https?://(hubdrive|drivehub|katdrive|kolop|drivefire)\.\S+', url)
+    url = re_match(r'https?://(hubdrive|drivehub|katdrive|kolop|drivefire|drivebuzz)\.\S+', url)
     return bool(url)
 
 def is_sharer_link(url: str):
